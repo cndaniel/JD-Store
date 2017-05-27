c976f614af42cd03f939eab0340ad07c41eb433b
伯桐 <wudawuxiao1375@gmail.com>
2017-05-27 17:28:25 +0800

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,19 +22,10 @@ end
 # Initialize product
 
 Product.create!(title: "冬茸",
-  description: "产自风光无限的竹塘乡蓦乃村
-                隶属于澜沧拉祜族自治县
-                远山如黛，人家参差，薄雾笼罩
-                这里便是冬茸的故乡
-                拉祜村民精心人工培植
-                清晨采摘，经烤箱烘至半干后自然晒干
-                保留了这份清晨的美味和营养
-                将这份美味送上餐桌
-                炒肉和煲汤味道绝佳
-                ",
-  price: 10,
-  quantity: 5,
-  image: open("https://ws1.sinaimg.cn/large/006tNbRwgy1ffxmhe8mz5j31kw14h1kz.jpg"),
+  description: "属大球盖菇，富含氨基酸、维生素等多种物质",
+  price: 98,
+  quantity: 98,
+  image: open("https://ooo.0o0.ooo/2017/05/27/59294543a294f.jpg"),
   particulars: "隶属于澜沧拉祜族自治县
                 远山如黛，人家参差，薄雾笼罩
                 这里便是冬茸的故乡
@@ -42,59 +33,11 @@ Product.create!(title: "冬茸",
                 清晨采摘，经烤箱烘至半干后自然晒干
                 保留了这份清晨的美味和营养
                 将这份美味送上餐桌
-                炒肉和煲汤味道绝佳
-  < img alt=\"\" src=\"https://ooo.0o0.ooo/2017/05/26/5927f6b99149e.jpg\">",
+                炒肉和煲汤味道绝佳"
+                https://ooo.0o0.ooo/2017/05/27/592945d669837.jpg
+                https://ooo.0o0.ooo/2017/05/27/5929460f93e16.jpg
+                https://ooo.0o0.ooo/2017/05/27/592945e2d0b3a.jpg,
   category: "源系",
   )
 
-  Product.create!(title: "冬茸",
-    description: "产自风光无限的竹塘乡蓦乃村
-                  隶属于澜沧拉祜族自治县
-                  远山如黛，人家参差，薄雾笼罩
-                  这里便是冬茸的故乡
-                  拉祜村民精心人工培植
-                  清晨采摘，经烤箱烘至半干后自然晒干
-                  保留了这份清晨的美味和营养
-                  将这份美味送上餐桌
-                  炒肉和煲汤味道绝佳
-                  ",
-    price: 10,
-    quantity: 5,
-    image: open("https://ws1.sinaimg.cn/large/006tNbRwgy1ffxmhe8mz5j31kw14h1kz.jpg"),
-    particulars: "隶属于澜沧拉祜族自治县
-    远山如黛，人家参差，薄雾笼罩
-    这里便是冬茸的故乡
-    拉祜村民精心人工培植
-    清晨采摘，经烤箱烘至半干后自然晒干
-    保留了这份清晨的美味和营养
-    将这份美味送上餐桌
-    炒肉和煲汤味道绝佳
-    < img alt=\"\" src=\"https://ooo.0o0.ooo/2017/05/26/5927f6b99149e.jpg\">",
-    category: "润系",
-    )
-
-    Product.create!(title: "冬茸",
-      description: "产自风光无限的竹塘乡蓦乃村
-                    隶属于澜沧拉祜族自治县
-                    远山如黛，人家参差，薄雾笼罩
-                    这里便是冬茸的故乡
-                    拉祜村民精心人工培植
-                    清晨采摘，经烤箱烘至半干后自然晒干
-                    保留了这份清晨的美味和营养
-                    将这份美味送上餐桌
-                    炒肉和煲汤味道绝佳
-                    ",
-      price: 10,
-      quantity: 5,
-      image: open("https://ws1.sinaimg.cn/large/006tNbRwgy1ffxmhe8mz5j31kw14h1kz.jpg"),
-      particulars: "隶属于澜沧拉祜族自治县
-      远山如黛，人家参差，薄雾笼罩
-      这里便是冬茸的故乡
-      拉祜村民精心人工培植
-      清晨采摘，经烤箱烘至半干后自然晒干
-      保留了这份清晨的美味和营养
-      将这份美味送上餐桌
-      炒肉和煲汤味道绝佳
-      < img alt=\"\" src=\"https://ooo.0o0.ooo/2017/05/26/5927f6b99149e.jpg\">",
-      category: "臻系",
-      )
+  
